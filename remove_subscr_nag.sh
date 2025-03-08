6401340cac3866fe79eef39a1de7f6d327006726
Bloodpack <kurierdienst.zimmermann@googlemail.com>
2025-03-08 23:50:22 +0100

    update 08.03.2025

--- remove_subscr_nag.sh
+++ remove_subscr_nag.sh
@@ -5,6 +5,7 @@ PROXMOX_DIR="/usr/share/javascript/proxmox-widget-toolkit"
 FILE="proxmoxlib.js"
 BACKUP_FILE="proxmoxlib.js.bak"
 TARGET_LINE="if (false) {"
+CHANGE_MADE=false
 
 # Switch to the directory where the file is stored
 cd "$PROXMOX_DIR" || { echo "Directory not found: $PROXMOX_DIR"; exit 1; }
@@ -20,11 +21,16 @@ else
     # Edit the file using sed to replace the line if it's not already replaced
     sed -i '/if (res === null || res === undefined || !res || res\.data\.status\.toLowerCase() !== "active") {/c\if (false) {' "$FILE" || { echo "Failed to edit $FILE"; exit 1; }
     echo "Line replaced successfully in $FILE"
+    CHANGE_MADE=true
 fi
 
-# Restart the PVE service to apply changes
-systemctl restart pveproxy || { echo "Failed to restart pveproxy"; exit 1; }
-echo "PVE service restarted successfully."
+# Restart the PVE service to apply changes, only if a change was made
+if $CHANGE_MADE; then
+    systemctl restart pveproxy || { echo "Failed to restart pveproxy"; exit 1; }
+    echo "PVE service restarted successfully."
+else
+    echo "No changes made. PVE service not restarted."
+fi
 
 # Exit the script
 exit 0
